57e0c541fc9eccddac104eb0cab17d09279e2a1a
mharris <mharris@rsglab.com>
2019-02-07 15:26:13 -0500

    get rid of duplicate action.

--- lib/action/actions.dart
+++ lib/action/actions.dart
@@ -1,20 +1,6 @@
 import 'package:imgsrc/model/gallery_item.dart';
 import 'package:imgsrc/model/gallery_models.dart';
 
-class LoadGalleryAction {
-  GallerySection section;
-  GallerySort sort;
-  GalleryWindow window;
-  int page;
-
-  LoadGalleryAction(this.section, this.sort, this.window, int page);
-
-  @override
-  String toString() {
-    return 'LoadGalleryAction{section: $section, sort: $sort, window: $window, page: $page';
-  }
-}
-
 class UpdateFilterAction {
   final GalleryFilter newFilter;
 
