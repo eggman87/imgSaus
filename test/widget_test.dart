14c204eed0cc19a42bd97148fb90e27b5ac73db9
Matthew Harris <maharris.us@gmail.com>
2019-02-13 21:37:43 -0500

    fix text

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:imgsrc/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(ReduxApp());
 
   });
 }
