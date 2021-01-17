cf901939d14d744af143ddf9894db5545254f827
Matthew Harris <maharris.us@gmail.com>
2021-01-16 21:25:23 -0600

    show indentation color on comments. limit author length.

--- lib/ui/comments_list.dart
+++ lib/ui/comments_list.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_linkify/flutter_linkify.dart';
 import 'package:imgsrc/model/comment_models.dart';
@@ -7,7 +9,8 @@ import 'package:timeago/timeago.dart';
 class CommentsList extends StatelessWidget {
   final List<CommentViewItem> commentItems;
 
-  CommentsList({Key key, @required List<Comment> comments}): this.commentItems = CommentViewItem._flattenComments(comments, 0);
+  CommentsList({Key key, @required List<Comment> comments})
+      : this.commentItems = CommentViewItem._flattenComments(comments, 0);
 
   int _commentsLength() {
     if (commentItems.length > 0) {
@@ -19,7 +22,9 @@ class CommentsList extends StatelessWidget {
 
   Widget _photoOrWebView(String url) {
     String lowerUrl = url.toLowerCase();
-    if (lowerUrl.contains(".jpg") || (url.contains(".gif") && !url.contains(".gifv")) || url.contains(".png")) {
+    if (lowerUrl.contains(".jpg") ||
+        (url.contains(".gif") && !url.contains(".gifv")) ||
+        url.contains(".png")) {
       return Image.network(url);
     } else {
       return WebView(
@@ -57,41 +62,32 @@ class CommentsList extends StatelessWidget {
               CommentViewItem viewItem = commentItems[index];
               Comment comment = viewItem.comment;
 
+              final indentColorBase = max((-5 + viewItem.indentLevel).abs(), 1);
+              var levelColor = Color((indentColorBase * 0xFFEEEE).toInt() << 0)
+                  .withOpacity(1.0);
+
               return Column(
                 children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.fromLTRB(16 * viewItem.indentLevel.toDouble(), 0, 0, 0),
-                    child: Column(
-                      children: <Widget>[
-                        Container(
-                          child: Linkify(
-                            text: (comment.comment),
-                            onOpen: (url) => _onUrlTapped(context, url),
-                          ),
-                          padding: EdgeInsets.fromLTRB(8, 8, 8, 0),
-                          alignment: Alignment(-1.0, -1.0),
-                        ),
-                        Container(
-                          child: Row(children: <Widget>[
-                            Text(comment.author,
-                                style: TextStyle(color: Colors.redAccent, fontWeight: FontWeight.bold)),
-                            Text(
-                              " ${format(comment.dateCreated, locale: "en_short")}",
-                              style: TextStyle(color: Colors.red.shade300, fontStyle: FontStyle.italic),
-                            ),
-                            Spacer(),
-                            Text(
-                              comment.points.toString(),
-                              textAlign: TextAlign.right,
-                              style: TextStyle(fontStyle: FontStyle.italic, color: Colors.red),
-                            )
-                          ]),
-                          padding: EdgeInsets.fromLTRB(8, 2, 4, 4),
-                          alignment: Alignment(-1.0, 0),
-                        )
-                      ],
-                    ),
+                  Divider(
+                    color: Colors.black26,
+                    height: .1,
                   ),
+                  IntrinsicHeight(
+                    child: Row(children: [
+                      Container(
+                        width: 9 * viewItem.indentLevel.toDouble(),
+                        color: levelColor,
+                      ),
+                      Expanded(
+                        child: Column(
+                          children: <Widget>[
+                            _commentBody(context, comment),
+                            _authorLine(comment),
+                          ],
+                        ),
+                      ),
+                    ]),
+                  )
                 ],
               );
             } else {
@@ -106,6 +102,47 @@ class CommentsList extends StatelessWidget {
           itemCount: _commentsLength(),
         ));
   }
+
+  Widget _commentBody(BuildContext context, Comment comment) {
+    return Container(
+      child: Linkify(
+        text: (comment.comment),
+        onOpen: (url) => _onUrlTapped(context, url),
+      ),
+      padding: EdgeInsets.fromLTRB(8, 8, 8, 0),
+      alignment: Alignment(-1.0, -1.0),
+    );
+  }
+
+  //todo: figure out how to make this more dymnamic in flutter world
+  final authorMaxLength = 30;
+
+  Widget _authorLine(Comment comment) {
+    var authorText = comment.author;
+    if (authorText.length > authorMaxLength) {
+      authorText = authorText.substring(0, authorMaxLength) + "...";
+    }
+    return Container(
+      child: Row(children: <Widget>[
+        Text(authorText,
+            style: TextStyle(
+                color: Colors.redAccent, fontWeight: FontWeight.bold)),
+        Text(
+          " ${format(comment.dateCreated, locale: "en_short")}",
+          style: TextStyle(
+              color: Colors.red.shade300, fontStyle: FontStyle.italic),
+        ),
+        Spacer(),
+        Text(
+          comment.points.toString(),
+          textAlign: TextAlign.right,
+          style: TextStyle(fontStyle: FontStyle.italic, color: Colors.red),
+        )
+      ]),
+      padding: EdgeInsets.fromLTRB(8, 2, 4, 4),
+      alignment: Alignment(-1.0, 0),
+    );
+  }
 }
 
 class CommentViewItem {
@@ -114,7 +151,8 @@ class CommentViewItem {
 
   CommentViewItem(this.comment, this.indentLevel);
 
-  static List<CommentViewItem> _flattenComments(List<Comment> comments, int indentLevel) {
+  static List<CommentViewItem> _flattenComments(
+      List<Comment> comments, int indentLevel) {
     if (comments == null || comments.length == 0) {
       return List();
     }
@@ -123,7 +161,8 @@ class CommentViewItem {
     for (var comment in comments) {
       flattenedComments.add(CommentViewItem(comment, indentLevel));
       if (comment.replies.length > 0) {
-        flattenedComments.addAll(_flattenComments(comment.replies, indentLevel + 1));
+        flattenedComments
+            .addAll(_flattenComments(comment.replies, indentLevel + 1));
       }
     }
     return flattenedComments;
